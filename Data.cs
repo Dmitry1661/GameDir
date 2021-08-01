f6edecd7a9bc5816db4d3c433755b2fad4c509ed
joinsmile555@gmail.com <joinsmile555@gmail.com>
2021-08-01 19:10:31 +0700

    free

--- Data.cs
+++ Data.cs
@@ -4,12 +4,12 @@
 public struct Data
 {
     /// <summary>
-    /// Имя обьекта, на котором висит скрипт GameManager.
+    /// Имя обьекта, на котором висит скрипт GameDir.
     /// </summary>
     public const string GAME_DIR = "GameDir";
 
     /// <summary>
-    /// Имя обьекта, на котором висит скрипт GameManager.
+    /// Имя обьекта, на котором висит скрипт GameLogic.
     /// </summary>
     public const string GAME_LOGIC = "GameLogic";
 }
